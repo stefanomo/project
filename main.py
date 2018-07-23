8d7e647517135aec9e1c691d8b53584921fc1547
yunyss <eunice6631@gmail.com>
2018-07-23 07:29:24 -0700

    text

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-"""Simple example app to demonstrate storing info for users.
-
-CSSI-ers!  If you want to have users log in to your site and store
-info about them, here is a simple AppEngine app demonstrating
-how to do that.  The typical usage is:
-
-- First, user visits the site, and sees a message to log in.
-- The user follows the link to the Google login page, and logs in.
-- The user is redirected back to your app's signup page to sign
-  up.
-- The user then gets a page thanking them for signup.
-
-- In the future, whenever the user is logged in, they'll see a 
-  message greeting them by name.
-
-Try logging out and logging back in with a fake email address
-to create a different account (when you "log in" running your
-local server, it doesn't ask for a password, and you can make
-up whatever email you like).
-
-The key piece that makes all of this work is tying the datastore
-entity to the AppEngine user id, by passing the special property
-id when creating the datastore entity.
-
-cssi_user = CssiUser(..., id=user.user_id())
-cssi_user.put()
-
-and then, looking it up later by doing
-
-cssi_user = CssiUser.get_by_id(user.user_id())
-"""
 
 import webapp2
 
@@ -36,20 +5,12 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 
 class CssiUser(ndb.Model):
-  """CssiUser stores information about a logged-in user.
-
-  The AppEngine users api stores just a couple of pieces of
-  info about logged-in users: a unique id and their email address.
-
-  If you want to store more info (e.g. their real name, high score,
-  preferences, etc, you need to create a Datastore model like this
-  example).  
-  """
   first_name = ndb.StringProperty()
   last_name = ndb.StringProperty()
   time = ndb.StringProperty()
   group_size = ndb.StringProperty()
-
+  preffered_seat = ndb.StringProperty()
+  
 class MainHandler(webapp2.RequestHandler):
   def get(self):
     user = users.get_current_user()
@@ -76,6 +37,7 @@ class MainHandler(webapp2.RequestHandler):
             <input type="text" name="last_name">
             <input type="text" name="time">
             <input type="text" name="group_size">
+            <input type="text" name="preffered_seat">
             <input type="submit">
             </form><br> %s <br>
             ''' % (email_address, signout_link_html))
@@ -96,6 +58,7 @@ class MainHandler(webapp2.RequestHandler):
         last_name=self.request.get('last_name'),
         time=self.request.get('time'),
         group_size=self.request.get('group_size'),
+        preffered_sear=self.request.get('preffered_seat'),
         id=user.user_id())
     cssi_user.put()
     self.response.write("Thanks for signing up, your reservation is at" + str(cssi_user.time) + "with a group size of" + str(cssi_user.group_size))
