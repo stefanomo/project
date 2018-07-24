6579a8ee6ed7e2c6c21ae147147d4a4ddd230d7f
yunyss <eunice6631@gmail.com>
2018-07-23 22:27:11 -0400

    needs more menu

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 
 import webapp2
+import jinja2
+import os
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
+from datetime import datetime
 
-class CssiUser(ndb.Model):
+class ReserveHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        a_template = the_jinja_env.get_template('index.html')
+        self.response.out.write(a_template.render())
+
+class AppUser(ndb.Model):
   first_name = ndb.StringProperty()
   last_name = ndb.StringProperty()
-  time = ndb.StringProperty()
-  group_size = ndb.StringProperty()
-  preffered_seat = ndb.StringProperty()
+  time = ndb.DateTimeProperty()
+  group_size = ndb.IntegerProperty()
   
 class MainHandler(webapp2.RequestHandler):
   def get(self):
@@ -17,30 +29,21 @@ class MainHandler(webapp2.RequestHandler):
     # If the user is logged in...
     if user:
       email_address = user.nickname()
-      cssi_user = CssiUser.get_by_id(user.user_id())
+      app_user = AppUser.get_by_id(user.user_id())
       signout_link_html = '<a href="%s">sign out</a>' % (
           users.create_logout_url('/'))
       # If the user has previously been to our site, we greet them!
-      if cssi_user:
+      if app_user:
         self.response.write('''
             Welcome %s %s (%s)! <br> %s <br>''' % (
-              cssi_user.first_name,
-              cssi_user.last_name,
+              app_user.first_name,
+              app_user.last_name,
               email_address,
               signout_link_html))
       # If the user hasn't been to our site, we ask them to sign up
       else:
-        self.response.write('''
-            Welcome to our site, %s!  Please sign up! <br>
-            <form method="post" action="/">
-            <input type="text" name="first_name">
-            <input type="text" name="last_name">
-            <input type="text" name="time">
-            <input type="text" name="group_size">
-            <input type="text" name="preffered_seat">
-            <input type="submit">
-            </form><br> %s <br>
-            ''' % (email_address, signout_link_html))
+        a_template = jinja_env.get_template('index.html')
+        self.response.out.write(a_template.render())
     # Otherwise, the user isn't logged in!
     else:
       self.response.write('''
@@ -53,16 +56,35 @@ class MainHandler(webapp2.RequestHandler):
       # You shouldn't be able to get here without being logged in
       self.error(500)
       return
-    cssi_user = CssiUser(
+    app_user = AppUser(
         first_name=self.request.get('first_name'),
         last_name=self.request.get('last_name'),
-        time=self.request.get('time'),
-        group_size=self.request.get('group_size'),
-        preffered_sear=self.request.get('preffered_seat'),
+        time=datetime.strptime(self.request.get('time'), "%m/%d/%Y %I:%M%p"),
+        group_size=int(self.request.get('group_size')),
         id=user.user_id())
-    cssi_user.put()
-    self.response.write("Thanks for signing up, your reservation is at" + str(cssi_user.time) + "with a group size of" + str(cssi_user.group_size))
+        
+    app_user.put()
+    self.response.write("Thanks for signing up, your reservation is at" + str(app_user.time)  +  "with a group size of"  +  str(app_user.group_size))
+
+class RegisterHandler(webapp2.RequestHandler):
+  def get(self):
+    a_template = jinja_env.get_template('register.html')
+    self.response.write(a_template.render())
+
+class ReserveHandler(webapp2.RequestHandler):
+  def get(self):
+    a_template = jinja_env.get_template('reserve.html')
+    self.response.write(a_template.render())
+    
+class HoursHandler(webapp2.RequestHandler):
+  def get(self):
+    a_template = jinja_env.get_template('location.html')
+    self.response.write(a_template.render())
+
 
 app = webapp2.WSGIApplication([
-('/', MainHandler)],
+('/', MainHandler),
+('/register', RegisterHandler),
+('/hours', HoursHandler),
+('/reserve', ReserveHandler)],
 debug=True)
